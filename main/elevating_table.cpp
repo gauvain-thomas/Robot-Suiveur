9aad8ea803eb822884e0ad0f256a8f71251d0d82
Squidoss <47402800+Squidoss@users.noreply.github.com>
2019-05-17 14:42:41 +0200

    Update elevating_table.cpp

--- main/elevating_table.cpp
+++ main/elevating_table.cpp
@@ -18,3 +18,22 @@ void update_table() {
   bool = digitalRead(pinIR2);
   bool = digitalRead(pinIR1);
 }
+
+void table_up() {
+  Serial.println("UP");
+  analogWrite(speedMotor, 255);
+
+  // turn on motor
+  digitalWrite(in1, HIGH);
+  digitalWrite(in2, LOW);
+  
+}
+
+void table_down() {
+  Serial.println("DOWN");
+  analogWrite(speedMotor, 255);
+
+  // turn on motor
+  digitalWrite(in1, LOW);
+  digitalWrite(in2, HIGH);  
+}
