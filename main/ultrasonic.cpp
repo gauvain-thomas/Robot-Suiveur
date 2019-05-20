dff3a261370fd0ce497c5434a459275ec78b2449
Squidoss <47402800+Squidoss@users.noreply.github.com>
2019-05-20 23:55:12 +0200

    Update

--- main/ultrasonic.cpp
+++ main/ultrasonic.cpp
@@ -58,6 +58,7 @@ char check_direction() {
   float angle;
   int sensor_dist = 100 ; // Distance between sensors (mm)
   float sound_speed = 0,343; // Speed of sound (mm/µs)
+  int d = 200 //Distance between sensors (mm)
   // long gauche = 0;
   // long droite = 0;
   // long compteur = 0;
@@ -119,8 +120,8 @@ time2 = pulseIn(pinEcho2, HIGH);
 d1 = time1 * sound_speed;
 d2 = time2 * sound_speed;
 
-distance = sqrt((sq(d1) - sq(d2)) / 4 );
-angle = pi - acos((sq(d1) - sq(d2)) / (sensor_dist * sqrt(2*sq(d1) + 2*sq(d2) - sq(sensor_dist))));
+distance = ( sqrt(sq(d1) +sq(d2) - sq(d) ) )/2;
+angle = acos( (sq(distance) + sq(d2) + sq(d/2))/(d*distance) );
 
 Serial.println(distance);
 Serial.println(angle);
@@ -140,35 +141,4 @@ else {
 
 delay(10);
 return direction;
-}
-
-char check_direction() {
-  const float pi = 3,1415926535;
-  char direction;
-  float distance;
-  float angle;
-  int sensor_dist = 100 ; // Distance between sensors (mm)
-  float sound_speed = 0,343; // Speed of sound (mm/µs)
-
-  Serial.println("Checking direction...");
-
-  while( (micros() - time_sync) % interval) {}
-  digitalWrite(pinTrig1, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(pinTrig1, LOW);
-  time1 = pulseIn(pinEcho1, HIGH);
-
-  while( (micros() - time_sync) % interval) {}
-  digitalWrite(pinTrig1, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(pinTrig1, LOW);
-  time2 = pulseIn(pinEcho1, HIGH);
-/*
-  while( (micros() - time_sync) % interval) {}
-  digitalWrite(pinTrig1, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(pinTrig1, LOW);
-  time3 = pulseIn(pinEcho1, HIGH);
-*/
-
 }
